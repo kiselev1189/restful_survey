18dd63a152acceb4e925bad918f89075ec5325ec
kiselev1189 <kiselev1189@gmail.com>
2015-09-18 23:35:57 +0300

    urls

--- survey/urls.py
+++ survey/urls.py
@@ -1,3 +1,11 @@
 __author__ = 'kis'
+from rest_framework.routers import DefaultRouter
+from survey import views
 
-
+router = DefaultRouter()
+router.register('survey_list', views.SurveyViewSet)
+router.register('question_list', views.QuestionViewSet)
+router.register('response_list', views.ResponseViewSet, base_name='Response')
+router.register('user_list', views.UserViewSet)
+router.register('answer_list', views.AnswerViewSet)
+survey_app_router = router
