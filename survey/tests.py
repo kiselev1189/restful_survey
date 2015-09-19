68f2561e7581c07ba671dcca26abf56542d1b624
kiselev1189 <kiselev1189@gmail.com>
2015-09-19 17:43:42 +0300

    urls

--- survey/tests.py
+++ survey/tests.py
@@ -1,12 +1,54 @@
 from django.test import TestCase
-
-
-
 from django.test import Client
-c = Client()
-response = c.post('api-auth/login/', {'username':'test_user', 'password':'111'})
-print(response.status_code)
-payload = {"question_1":"sending some text on question 1...", "question_2":"text to question 2"}
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+import factory
+from django.contrib.auth.models import User
+from rest_framework import status
+from .models import Survey, Question, Answer, Response
 
-#r = s.post("http://127.0.0.1:8000/survey_list/1/", data=payload, cookies=cookies)
-#print(r.text)
+
+
+class SurveyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Survey
+        django_get_or_create = ("name",)
+
+    name = 'Default Survey Name'
+    description = "Default Survey Description"
+
+
+class QuestionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Question
+
+    survey = factory.SubFactory(SurveyFactory)
+    question_type = Question.TEXT
+    text = "Default question text?"
+    label = factory.Sequence(lambda n: "question_" + str(n) )
+
+
+
+
+class SurveyTestCase(APITestCase):
+    def setUp(self):
+        self.survey = SurveyFactory()
+        # get 3 phone fields, 3 text fields
+
+        question_text = QuestionFactory(survey=self.survey, question_type=Question.TEXT, label="question_text")
+        question_phone = QuestionFactory(survey=self.survey, question_type=Question.PHONE, label="question_phone")
+
+
+    def test_respond(self):
+        user = User.objects.create_user("default_username", "mail@mail.com", "default_password")
+
+        c = self.client.login(username="default_username", password="default_password")
+        payload = {"question_text":"sending some text on question 1...", "question_phone":"+999999999"}
+        url = "http://testserver/survey_list/" + str(self.survey.id)
+        response = self.client.post(url, payload, format="json")
+        print(response)
+        self.assertEqual(Response.objects.count(), 1)
+        self.assertEqual(Answer.objects.count(), 2)
+
+
+
