76adae2546053436c5f04a53b68117b1ac2ac2e8
kiselev1189 <kiselev1189@gmail.com>
2015-09-19 18:02:58 +0300

    urls

--- survey/tests.py
+++ survey/tests.py
@@ -35,18 +35,18 @@ class SurveyTestCase(APITestCase):
         self.survey = SurveyFactory()
         # get 3 phone fields, 3 text fields
 
-        question_text = QuestionFactory(survey=self.survey, question_type=Question.TEXT, label="question_text")
-        question_phone = QuestionFactory(survey=self.survey, question_type=Question.PHONE, label="question_phone")
 
 
     def test_respond(self):
         user = User.objects.create_user("default_username", "mail@mail.com", "default_password")
+        question_text = QuestionFactory(survey=self.survey, question_type=Question.TEXT, label="text")
+        question_phone = QuestionFactory(survey=self.survey, question_type=Question.PHONE, label="phone")
 
-        c = self.client.login(username="default_username", password="default_password")
+        self.client.login(username="default_username", password="default_password")
         payload = {"question_text":"sending some text on question 1...", "question_phone":"+999999999"}
-        url = "http://testserver/survey_list/" + str(self.survey.id)
-        response = self.client.post(url, payload, format="json")
-        print(response)
+        url = "/survey_list/" + str(self.survey.id)
+        response = self.client.get(url)
+        response = self.client.post(response.url, payload, format="json")
         self.assertEqual(Response.objects.count(), 1)
         self.assertEqual(Answer.objects.count(), 2)
 
